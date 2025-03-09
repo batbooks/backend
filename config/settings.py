9381b098d74c6d1cd66dd9c11f50351cfb3cacd8
mohammadrazeghi <117801748+mohammadrazeghi@users.noreply.github.com>
2025-03-10 00:04:21 +0330

    secrect key change

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import dotenv
-import  os
+import os
 
 ENV = dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c5br57^imn$9=pen&bwevh7u)ip5^!#+$spk6h-%jh=skhpx9r'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
