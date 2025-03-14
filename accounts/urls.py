aabe2ca235f5c66c545bc867bac0d8549fb26d1c
mohammadrazeghi <117801748+mohammadrazeghi@users.noreply.github.com>
2025-03-15 00:34:07 +0330

    small change to url and complete reset and password

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from .views import *
+
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='home'),
     path('otp/',views.VerifyOTPView.as_view(), name='otp'),
-    path('reset-password/send-otp/', SendOTPResetView.as_view(), name='send-reset-otp'),
-    path('reset-password/verify-otp/', VerifyOTPAndResetPasswordView.as_view(), name='verify-reset-otp'),
+    path('reset-password/send-otp/', views.SendOTPResetView.as_view(), name='send-reset-otp'),
+    path('reset-password/verify-otp/', views.VerifyOTPAndResetPasswordView.as_view(), name='verify-reset-otp'),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
