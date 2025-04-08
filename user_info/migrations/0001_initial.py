1da8a84885f5c0466e08599306da487980c09b2b
dark-noob830 <mahdiamr83@gmail.com>
2025-04-09 00:48:57 +0330

    change git ignore

--- user_info/migrations/0001_initial.py
+++ user_info/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
+    initial = Truedasf
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
