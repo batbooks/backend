3f4ec0d958297b264e30d87f9f77f8e3b105c254
mohammadrazeghi <117801748+mohammadrazeghi@users.noreply.github.com>
2025-04-23 16:43:42 +0330

    fixing the problem the books not being sorted

--- book/serializers.py
+++ book/serializers.py
@@ -86,8 +86,8 @@ class BookGetAllSerializer(serializers.ModelSerializer):
     tags = serializers.StringRelatedField(many=True)
 
     def get_chapters(self, obj):
-        res = obj.chapters.filter(is_approved=True)
-        return ChapterGetSerializer(instance=res, many=True).data
+        res = obj.chapters.filter(is_approved=True).order_by('created_at')
+        return ChapterSummarySerializer(instance=res, many=True).data
 
     class Meta:
         model = Book
@@ -130,6 +130,26 @@ class ChapterGetSerializer(serializers.ModelSerializer):
 
     def get_Author(self, obj):
         return obj.book.Author.name
+
+class ChapterSummarySerializer(serializers.ModelSerializer):
+    book = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    Author = serializers.SerializerMethodField()
+    book_image = serializers.SerializerMethodField()
+    rating = serializers.SerializerMethodField()
+
+    def get_rating(self, obj):
+        return obj.book.rating_avg
+
+    def get_book_image(self, obj):
+        return obj.book.image.url if obj.book.image else None
+
+    def get_Author(self, obj):
+        return obj.book.Author.name
+
+    class Meta:
+        model = Chapter
+        exclude = ['body']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'book', 'book_image']
 
 
 class ChapterCreateSerializer(serializers.ModelSerializer):
