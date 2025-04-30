03e5f523c3fba1f6bf5106ffb0bd5d624190aa90
dark-noob830 <mahdiamr83@gmail.com>
2025-04-30 18:27:40 +0330

    edit entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,4 @@ python manage.py migrate --noinput
 echo ">> Collecting static files"
 python manage.py collectstatic --noinput
 
-echo ">> Loading initial fixtures (optional)"
-python run_fixture.py || echo "Fixtures not loaded!"
-
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
