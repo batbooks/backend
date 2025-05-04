957fca7a11e00187a51f35574749b1933871ae55
dark-noob830 <mahdiamr83@gmail.com>
2025-05-04 17:34:50 +0330

    tag id for comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -8,7 +8,10 @@ class CommentSerializer(serializers.ModelSerializer):
     reply_count = serializers.SerializerMethodField()
     image = serializers.SerializerMethodField()
     user = serializers.SerializerMethodField()
+    tag_id = serializers.SerializerMethodField()
 
+    def get_tag_id(self, obj):
+        return obj.tag.id
     class Meta:
         model = Comment
         fields = '__all__'
@@ -47,6 +50,7 @@ class ReplyCommentSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'chapter':{'read_only':True},
             'user': {'read_only': True},
+            'tag_id': {'read_only': True},
         }
 
     def get_image(self, obj):
