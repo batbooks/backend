1e54154314bccb60bb3d5dbdfabb16112c305c85
dark-noob830 <mahdiamr83@gmail.com>
2025-05-04 16:40:41 +0330

    seting of nginx

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
