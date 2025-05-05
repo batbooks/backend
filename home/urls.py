1cba7eaa0f924aaacaabfd6179f33a709742a77c
dark-noob830 <mahdiamr83@gmail.com>
2025-05-05 15:54:39 +0330

    homepage complete

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.urls import  path
 from . import  views
 urlpatterns=[
     path('category/',views.HomeCategoryView.as_view(),name='home'),
+    path('suggestion/book/',views.HomeSuggestionBookView.as_view(),name='suggestion_book'),
+    path('newest/book/',views.HomeNewestBookView.as_view(),name='newest_book'),
     path('popular/author/',views.HomeMostPopularAuthorsView.as_view(),name='popular'),
-    path('active/author/',views.HomeMostActiveAuthorsView.as_view(),name='popular'),
+    path('active/author/',views.HomeMostActiveAuthorsView.as_view(),name='active'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,18 +1,36 @@
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from book.models import Book
+from book.serializers import BookSerializer
 from .serializer import GenreSerializer
 from tag.models import Genre
 from django.db.models import Count
 from user_info.serializers import UserInfoSerializer
 from user_info.models import UserInfo
+from rest_framework import status
 
 
 class HomeCategoryView(APIView):
     def get(self, request):
         Genre_list = Genre.objects.annotate(book_count=Count('books')).order_by('-book_count')[:8]
         ser_data = GenreSerializer(Genre_list, many=True)
-        return Response(ser_data.data)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
+
+
+class HomeSuggestionBookView(APIView):
+    def get(self, request):
+        books = Book.objects.order_by('?')[:5]
+        ser_data = BookSerializer(books, many=True)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
+
+
+class HomeNewestBookView(APIView):
+    def get(self, request):
+        book = Book.objects.order_by('-created_at')[:5]
+        ser_data = BookSerializer(book, many=True)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
 
 
 class HomeMostPopularAuthorsView(APIView):
@@ -20,7 +38,7 @@ class HomeMostPopularAuthorsView(APIView):
     def get(self, request):
         author = UserInfo.objects.annotate(followed_count=Count('user__following')).order_by('-followed_count')[:6]
         ser_data = UserInfoSerializer(author, many=True)
-        return Response(ser_data.data)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
 
 
 class HomeMostActiveAuthorsView(APIView):
@@ -28,4 +46,4 @@ class HomeMostActiveAuthorsView(APIView):
     def get(self, request):
         author = UserInfo.objects.annotate(books=Count('user__books')).order_by('-books')[:6]
         ser_data = UserInfoSerializer(author, many=True)
-        return Response(ser_data.data)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
