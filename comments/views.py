32a32878e7f4e5700befa208d9170608cf15b82d
dark-noob830 <mahdiamr83@gmail.com>
2025-05-14 00:20:13 +0330

    setting and bug for username edit

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from math import floorF
+from math import floor
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
--- config/sett1ng/application.py
+++ config/sett1ng/application.py
@@ -50,6 +50,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 ASGI_APPLICATION = "config.asgi.application"
 
 AUTH_USER_MODEL = "accounts.User"
