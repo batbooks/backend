b9a06687c746df8c373e87d2636fb2f2c58f1088
mohammadrazeghi <117801748+mohammadrazeghi@users.noreply.github.com>
2025-05-14 17:47:16 +0330

    all user api

--- user_info/urls.py
+++ user_info/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('info/change/username/', views.UsernameUpdateView.as_view(), name='change-username'),
     path('info/change/update/',views.UserInfoUpdateView.as_view(), name='change-info'),
     path('search/<str:user_name>/', views.SearchUserView.as_view(), name='search-user'),
+    path('users/all/', views.AllUsersView.as_view(), name='all-users'),
     path('toggle/follow/<int:user_id>/',views.ToggleFollowUserView.as_view(), name='follow'),
     path('is/follow/<int:user_id>/',views.IsFollowUserView.as_view(), name='is-follow'),
     path('followers/', views.FollowersView.as_view(), name='followers'),
