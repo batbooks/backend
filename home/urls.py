5d90a7ff0c49eaa9ab90d49d50cfd3ccf1ba31eb
dark-noob830 <mahdiamr83@gmail.com>
2025-05-18 02:37:47 +0330

    nginx for domain

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
-from django.urls import  path
-from . import  views
-urlpatterns=[
-    path('category/',views.HomeCategoryView.as_view(),name='home'),
-    path('suggestion/book/',views.HomeSuggestionBookView.as_view(),name='suggestion_book'),
-    path('newest/book/',views.HomeNewestBookView.as_view(),name='newest_book'),
-    path('popular/author/',views.HomeMostPopularAuthorsView.as_view(),name='popular'),
-    path('active/author/',views.HomeMostActiveAuthorsView.as_view(),name='active'),
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path('category/', views.HomeCategoryView.as_view(), name='home'),
+    path('suggestion/book/', views.HomeSuggestionBookView.as_view(), name='suggestion_book'),
+    path('newest/book/', views.HomeNewestBookView.as_view(), name='newest_book'),
+    path('popular/author/', views.HomeMostPopularAuthorsView.as_view(), name='popular'),
+    path('active/author/', views.HomeMostActiveAuthorsView.as_view(), name='active'),
 ]
