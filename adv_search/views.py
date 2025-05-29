3a8fc89c42da5200489315d7f54897dcd039d9fe
dark-noob830 <mahdiamr83@gmail.com>
2025-05-30 00:49:08 +0330

    for advanced search

--- adv_search/views.py
+++ adv_search/views.py
@@ -4,15 +4,17 @@ from rest_framework import generics
 from book.serializers import BookSerializer
 from rest_framework import filters as drf_filters
 from .fIlters import BookFilter
-from django.db.models import F, ExpressionWrapper, DecimalField,Count,IntegerField
-from django.db.models.functions import Cast , Coalesce
+from django.db.models import F, ExpressionWrapper, DecimalField, Count, IntegerField
+from django.db.models.functions import Cast, Coalesce
 from django.db.models.functions import Coalesce
+
 
 class SearchView(generics.ListAPIView):
     queryset = Book.objects.annotate(
         avg_rating=ExpressionWrapper(
-            Cast(Coalesce(F('rating_sum'), 0), DecimalField()) /
-            Cast(Coalesce(F('rating_count'), 1), DecimalField()),  # جلوگیری از تقسیم بر صفر
+            Cast(Coalesce(F('rating_sum'), 0), DecimalField(max_digits=5, decimal_places=2), ) /
+            Cast(Coalesce(F('rating_count'), 1), DecimalField(max_digits=5, decimal_places=2)),
+            # جلوگیری از تقسیم بر صفر
             output_field=DecimalField()
         ),
         chapter_count=ExpressionWrapper(
